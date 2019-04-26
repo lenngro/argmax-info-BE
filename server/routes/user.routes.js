b36e2408008d4d59bee596ccd3866ff65fae9f53
Lennart Grosser <lennartgrosser13@web.de>
2019-04-26 11:30:22 +0200

    deleted server folder

--- server/routes/user.routes.js
+++
@@ -1,8 +0,0 @@
-module.exports = (app) => {
-
-    const users = require('../controllers/user.controller.js');
-
-    // Retrieve a single User with username
-    app.post('/users/:username', users.findOne);
-
-}
