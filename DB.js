9d6a35610717312f27f9f409ef587eb9a9696dec
Lennart Grosser <lennartgrosser13@web.de>
2019-04-26 13:24:42 +0200

    updated server config

--- DB.js
+++ DB.js
@@ -1,5 +1,5 @@
 
 module.exports = {
-    DB: "mongodb://lenngro:lg93--hu@argmax-info-o3xdy.mongodb.net/test?retryWrites=true",
+    DB: "mongodb+srv://lenngro:lg93--hu@argmax-info-o3xdy.mongodb.net/test?retryWrites=true",
     port: 8082
  };
