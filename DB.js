ac6e11b18816306a8083dc6a4b8e93480128fe2e
lenngro <32907710+lenngro@users.noreply.github.com>
2019-04-29 16:00:42 +0200

    Update DB.js

--- DB.js
+++ DB.js
@@ -1,4 +1,4 @@
 
 module.exports = {
-    url = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@argmax-info-o3xdy.mongodb.net/test?retryWrites=true`;
+    url = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@argmax-info-o3xdy.mongodb.net/test?retryWrites=true`
  };
