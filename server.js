b310f03841221495a28c30c12b8f595328e53787
Lennart Grosser <lennartgrosser13@web.de>
2019-05-03 13:24:50 +0200

    updated limit

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ app.use(cors())
 app.get('/', (req, res) => {
     res.json({"message": "Welcome to argmax.info's backend."});
 });
-
-
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 
 // link to defined routes
 require('./routes/post.routes')(app);
