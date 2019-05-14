d7957e93f8eddbd75a23a9f7cfb553025f972b9b
Lennart <Grosser>
2019-05-14 19:57:49 +0200

    corrected utils route

--- routes/utils.routes.js
+++ routes/utils.routes.js
@@ -3,6 +3,6 @@ module.exports = (app) => {
     const utils = require('../controllers/utils.controller.js');
 
     // Retrieve all Notes
-    app.get('/posts/list', posts.findAll);
+    app.get('/utils/list', utils.findAll);
 
 }
