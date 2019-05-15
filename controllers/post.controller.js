c4dfea7d3cc348e82b4eb77149779bfdb75eefd7
Lennart <Grosser>
2019-05-15 19:08:33 +0200

    adjusted find one

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -42,7 +42,7 @@ exports.findAll = (req, res) => {
 
 // Find a single note with a postId
 exports.findOne = (req, res) => {
-    Post.findById(req.params.postId)
+    Post.findOne({'title': req.body.content.title, 'url': req.body.content.url})
     .then(post => {
         if(!post) {
             return res.status(404).send({
